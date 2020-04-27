fc7c926763cdcb461fdb4d519f0ae3b0ecd00a15
David Akpan <daviduwana@gmail.com>
2020-04-27 12:44:38 +0100

    Updates

---
+++ precache-manifest.74498f67dc569350554a393551b5fac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9b8a67c52a8f72ca644f680e7500d5",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "e7b49ea0c205fef5d0d9",
+    "url": "/react-router/static/css/main.1e74a3bb.chunk.css"
+  },
+  {
+    "revision": "c308498072f6cbc2005f",
+    "url": "/react-router/static/js/2.41c2fbef.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-router/static/js/2.41c2fbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b49ea0c205fef5d0d9",
+    "url": "/react-router/static/js/main.2a78256f.chunk.js"
+  },
+  {
+    "revision": "382d1f0d9a077c8e52ef",
+    "url": "/react-router/static/js/runtime-main.51193319.js"
+  }
+]);
